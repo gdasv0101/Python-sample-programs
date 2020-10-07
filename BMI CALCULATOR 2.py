55b8d92a87500b960d24be31eb9f895b4bbf49fd
gdasv0101 <72097776+gdasv0101@users.noreply.github.com>
2020-10-07 04:01:56 -0700

    Add files via upload

---
+++ BMI CALCULATOR 2.py
@@ -0,0 +1,14 @@
+print("BODY MASS INDEX CALCULATOR")
+print(".......................................................................................")
+n=input("Enter your name :  ")
+a = float(input("Enter Body mass (In Kg) :"))#a=weight in kg
+b = float(input("Enter Body height (In m) :"))#b=height in m
+BMI=a/(b**2)
+if BMI<18.5:
+    print(n,"is Underweight by",BMI,"BMI")
+elif 18.6<= BMI <=24.9:
+    print(n,"is Normal by",BMI,"BMI")
+elif 25.0<= BMI <=30.9:
+    print(n,"is overweight by",BMI,"BMI")
+elif BMI>30:
+    print(n,"is obese by",BMI,"BMI")
